01e3699af28ad155f155c216786c554e358074b2
JAzzNoTE <jazznote82@gmail.com>
2025-11-16 12:22:41 +0800

    Add important annotation

--- libs/proto.js
+++ libs/proto.js
@@ -1,27 +1,30 @@
 const util = require('./util.js');
-const QueueSMA = require('./QueueSMA.js');
-const QueueEMA = require('./QueueEMA.js');
-const QueueVMA = require('./QueueVMA.js');
-const QueueRSI = require('./QueueRSI.js');
-const QueueKDJ = require('./QueueKDJ.js');
-const QueueMACD = require('./QueueMACD.js');
-const QueueBollinger = require('./QueueBollinger.js');
 
-const selector = {
-  sma: QueueSMA,
-  ema: QueueEMA,
-  vma: QueueVMA,
-  rsi: QueueRSI,
-  kdj: QueueKDJ,
-  macd: QueueMACD,
-  bband: QueueBollinger
+const Queue = {
+  sma: require('./QueueSMA.js'),
+  ema: require('./QueueEMA.js'),
+  vma: require('./QueueVMA.js'),
+  rsi: require('./QueueRSI.js'),
+  kdj: require('./QueueKDJ.js'),
+  macd: require('./QueueMACD.js'),
+  bband: require('./QueueBollinger.js')
 };
-// TODO: 寫註解: 將queue組合為指標的屬性，並包裏一層facade方法
+
+/**
+ * Base constructor for a financial indicator. This acts as a facade, wrapping a specific
+ * indicator queue (like SMA, EMA, etc.) based on the `this.type` property.
+ * @constructor
+ * @param {number} timeRange - The time period or range for the indicator calculation.
+ * @param {Array} [preItems] - Optional. An array of pre-existing data items to initialize the indicator queue.
+ * @throws {Error} If `this.type` is not specified on the inheriting constructor.
+ */
 function Indicator(timeRange, preItems) {
   if (!this.type) throw new Error('Indicator | this.type should be specified');
-  this.queue = new selector[this.type.toLowerCase()](timeRange, preItems);
+  if (!Object.keys(Queue).includes(this.type)) throw new Error(`finance-indicators ERROR | non-existent indicator type:${this.type}`);
+  this.queue = new Queue[this.type.toLowerCase()](timeRange, preItems);
 }
 
+// Standard and universal indicator methods
 Indicator.prototype = {
   enqueue: function (doc, isStickFinished) { return this.queue.enqueue(doc, isStickFinished) },
   now: function () { return this.queue.now() },
